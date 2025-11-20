bd58f6258e30a6712c357a4d2a29ebfcb4995aa7
SpektrunBackend <spektrunbackend@gmail.com>
2025-11-20 20:16:18 +0100

    V.1.1.5

--- utils/pdfMetadata.js
+++ utils/pdfMetadata.js
@@ -4,18 +4,24 @@ export async function addEndlessForgeMetadata(pdfDoc) {
   const producer = "Endless Forge PDF API";
   const customMessage = "Processed or generated using Endless Forge Website";
 
-  const infoDictRef = pdfDoc.context.trailer.get(PDFName.of("Info"));
-  let infoDict = infoDictRef ? pdfDoc.context.lookup(infoDictRef) : null;
+  const trailer = pdfDoc.context.trailer;
+  
+  if (!trailer || typeof trailer.get !== 'function') {
+      console.warn("pdfDoc.context.trailer is missing or invalid. Skipping internal metadata dictionary setup.");
+      
+  } else {
+      let infoDict = infoDictRef ? pdfDoc.context.lookup(infoDictRef) : null;
 
-  if (!infoDict) {
-    infoDict = pdfDoc.context.obj({});
-    pdfDoc.context.trailer.set(PDFName.of("Info"), infoDict);
+      if (!infoDict) {
+        infoDict = pdfDoc.context.obj({});
+        trailer.set(PDFName.of("Info"), infoDict);
+      }
+
+      infoDict.set(PDFName.of("Producer"), PDFString.of(producer));
+      infoDict.set(PDFName.of("Creator"), PDFString.of("Endless Forge"));
+      infoDict.set(PDFName.of("Comments"), PDFString.of(customMessage));
   }
-
-  infoDict.set(PDFName.of("Producer"), PDFString.of(producer));
-  infoDict.set(PDFName.of("Creator"), PDFString.of("Endless Forge"));
-  infoDict.set(PDFName.of("Comments"), PDFString.of(customMessage));
-
+  
   pdfDoc.setProducer(producer);
   pdfDoc.setCreator("Endless Forge");
   pdfDoc.setTitle("Generated PDF");
